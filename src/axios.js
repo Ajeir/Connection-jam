fc1b0fa501a6a31b95e88f5f53e47818fa10ef07
Ajeir Cunningham <dev0sarc@gmail.com>
2021-12-14 20:17:26 -0500

    connected to heroku backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://connection-jam.herokuapp.com/',
 });
 
 export default instance;
